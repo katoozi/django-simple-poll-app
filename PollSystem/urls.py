5bb21f2bf2c9d54c4a803548060b9d71eb47d323
mohammad katoozi <k2527806@gmail.com>
2019-03-12 11:04:20 +0330

    create urls.py file for Public app, include Public.urls in to main urls.py

--- PollSystem/urls.py
+++ PollSystem/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from Public import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^poll/', include('Poll.urls')),
-    url(r'^$', views.home),
+    url(r'^', include("Public.urls", namespace="public", app_name="Public")),
 ]
