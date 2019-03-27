f5c3f44cc1fa3031baf554b4eecd5c56996f0eb3
mohammad katoozi <k2527806@gmail.com>
2019-03-27 12:41:09 +0430

    add middleware for fix change language issue

---
+++ PollSystem/middleware/language_control.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from django.utils import translation
+from django.shortcuts import redirect
+
+
+class LanguageControl(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        url_list = request.get_full_path().split('/')
+        if url_list[1] == 'en' or url_list[1] == 'fa':
+            language = translation.get_language_from_request(request)
+            if url_list[1] == 'en' and language == 'en':
+                return self.get_response(request)
+            elif url_list[1] == 'fa' and language == 'fa':
+                return self.get_response(request)
+            translation.activate(language)
+            request.LANGUAGE_CODE = translation.get_language()
+            return redirect(request.get_full_path()[3:])
+        return self.get_response(request)
+
+
+class LanguageObject(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if 'HTTP_ACCEPT_LANGUAGE' in request.META:
+            del request.META['HTTP_ACCEPT_LANGUAGE']
+        return self.get_response(request)
